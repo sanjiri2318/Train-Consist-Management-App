ed098d468cf7e439414b01d1f1461098f558aa14
sanjiri2318 <ssanjith960@gmail.com>
2026-04-16 23:39:22 +0530

    UC12

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,1 +1,1 @@
-public class TrainConsistManagementApp{public static void main(String[] a){System.out.println("UC11");}} 
+public class TrainConsistManagementApp{public static void main(String[] a){System.out.println("UC12");}} 
